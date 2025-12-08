f759131ab3315183e54d00a2c104a74e101a3983
Zishwa <powerlinectg@gmail.com>
2025-12-08 21:36:44 +0600

    Add files via upload

---
+++ a1.py
@@ -0,0 +1,5 @@
+Price = 100
+price3= 200
+price1= 300
+V1= "I bought 3 items for the price of"
+print(V1, Price + price3 + price1, end='$')
---
+++ a2.py
@@ -0,0 +1,4 @@
+Age=input("Enter how many years you are younger than your father: ")
+sum=input("Enter the sum of your ages: ")
+print("Your father's age is", sum-Age)
+print("Your age is", sum-(sum-Age))
