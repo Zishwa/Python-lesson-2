d4e80b3a8c67bc64dbbf532ebf6b1529f02c4dc2
Zishwa <powerlinectg@gmail.com>
2025-12-15 20:08:14 +0600

    Add files via upload

--- a1.py
+++ a1.py
@@ -1,5 +1,10 @@
-Price = 100
-price3= 200
-price1= 300
-V1= "I bought 3 items for the price of"
-print(V1, Price + price3 + price1, end='$')
+
+birthdays = {
+    "Alice": "2005-04-12",
+    "Bob": "2004-11-23",
+    "Charlie": "2005-07-19",
+    "Diana": "2006-01-30",
+    "Ethan": "2005-09-05"
+}
+for name, bday in birthdays.items():
+    print(f"{name}'s birthday is on {bday}")
